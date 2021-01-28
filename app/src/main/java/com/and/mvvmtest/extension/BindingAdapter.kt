146d5c06b83b68abfa7abcc60da877bfba52978c
rilflatron1428 <flatron1428@gmail.com>
2021-01-28 22:37:11 +0900

    소스 수정

--- app/src/main/java/com/and/mvvmtest/extension/BindingAdapter.kt
+++ app/src/main/java/com/and/mvvmtest/extension/BindingAdapter.kt
@@ -4,9 +4,9 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.and.mvvmtest.adapter.SearchUiAdapter
 
-@BindingAdapter("setSearchData")
-fun setSearch(listview: RecyclerView, data: ArrayList<String>) {
-    listview?.adapter?.let {
-        (it as SearchUiAdapter).setData(data)
+@BindingAdapter("adapterData")
+fun setAdapterData(rcView :RecyclerView?, item :ArrayList<String>?) {
+    item?.let {
+        (rcView?.adapter as SearchUiAdapter).setData(item)
     }
 }
